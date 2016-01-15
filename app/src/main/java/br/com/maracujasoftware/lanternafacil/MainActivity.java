c7efa06c7707fc90e5eba53bf8b383e8a371c4fc
Julio Araujo <julio.araujo314@gmail.com>
2016-01-15 18:28:46 -0200

    comeco de implementação do código ativar

--- app/src/main/java/br/com/maracujasoftware/lanternafacil/MainActivity.java
+++ app/src/main/java/br/com/maracujasoftware/lanternafacil/MainActivity.java
@@ -1,15 +1,23 @@
 package br.com.maracujasoftware.lanternafacil;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btAtivar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +32,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Toast toast = Toast.makeText(MainActivity.this, "oi", Toast.LENGTH_LONG);
+                toast.show();
             }
         });
+       /* if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+        }
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                48);
+
+        btAtivar = (Button) findViewById(R.id.button_ativar);
+        btAtivar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Camera cam = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK);
+                cam.release();
+                Camera.Parameters p = cam.getParameters();
+                p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                cam.setParameters(p);
+                cam.startPreview();
+            }
+        });*/
+
     }
 
     @Override
